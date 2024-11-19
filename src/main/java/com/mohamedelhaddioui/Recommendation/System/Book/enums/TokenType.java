0acbf395cd48d3a4684e6add58c7ccba72e27a37
EL HADDIOUI MOHAMED <80369317+MOHAMED-EL-HADDIOUI@users.noreply.github.com>
2024-11-19 17:59:14 +0100

    second commit

--- src/main/java/com/mohamedelhaddioui/Recommendation/System/Book/enums/TokenType.java
+++
@@ -1,5 +0,0 @@
-package com.mohamedelhaddioui.Recommendation.System.Book.enums;
-
-public enum TokenType {
-    BEARER
-}
--- src/main/java/com/mohamedelhaddioui/Recommendation/System/Book/exceptions/BookNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.mohamedelhaddioui.Recommendation.System.Book.exceptions;
-
-public class BookNotFoundException extends  Exception{
-
-    public BookNotFoundException(String message) {
-        super(message);
-    }
-}
